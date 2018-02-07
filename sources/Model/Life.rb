8d6de5b6b3198353bcdd52a1f293c204aeb7141a
Alexpert <salmon.alexandre10@gmail.com>
2018-02-07 09:50:18 +0100

    La vie....

--- sources/Model/Life.rb
+++ sources/Model/Life.rb
@@ -6,5 +6,28 @@ class Life
 	def initialize(name)
 		@name = name
 		@age = 0
+
+		@art = 0
+		@doubt = 0
+		@intelligence = 0
+		@sport = 0
+		@cleanliness = 0
+		@instabity = 0
+		@wealth = 0
+		@violence = 0
+		@drugs = 0
+		@love = 0
+		@blackSheep = 0
+		@flemme = 0
+
+		@prison = false
+		@hospital = false
+		@dog = false
+		@university = false
+		@employment = false
+
+		@car = nil
+		@child = nil
+		@marriedTo = nil
 	end
 end
