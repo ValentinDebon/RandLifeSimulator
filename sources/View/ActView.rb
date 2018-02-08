73b3ceae913acc942593cc8b901d1cc9b6d56f72
Valentin Debon <valentin@debon.net>
2018-02-08 10:15:13 +0100

    ActScene

--- sources/View/ActView.rb
+++ sources/View/ActView.rb
@@ -9,22 +9,22 @@ class ActView
 		case @view.controller.act
 			when "I"
 				@view.playSound("View/Assets/Musics/Brittle Rille.mp3")
-				@subtitle = "\n- Enfance -"
+				@subImg = Gosu::Image.from_text("- Enfance -", 60)
 			when "II"
 				@view.playSound("View/Assets/Musics/Do I Wanna Know.mp3")
-				@subtitle = "\n- Adolescence -"
+				@subImg = Gosu::Image.from_text("- Adolescence -", 60)
 			when "III"
 				@view.playSound("View/Assets/Musics/Act Like You Know.mp3")
-				@subtitle = "\n- Adultescence -"
+				@subImg = Gosu::Image.from_text("- Adultescence -", 60)
 			when "IV"
 				@view.playSound("View/Assets/Musics/Nutcracker.mp3")
-				@subtitle = "\n- La retraite -"
+				@subImg = Gosu::Image.from_text("- La retraite -", 60)
 			else
 				@view.playSound(nil)
-				@subtitle = "\n- ? -"
+				@subImg = Gosu::Image.from_text("- ? -", 60)
 		end
 
-		@actImg = Gosu::Image.from_text("Acte " + @view.controller.act + @subtitle, 100)
+		@actImg = Gosu::Image.from_text("Acte " + @view.controller.act, 100)
 		@animStart = Gosu::milliseconds
 		@duration = 5000
 	end
@@ -46,6 +46,9 @@ class ActView
 	def draw
 		alpha = Float(Gosu::milliseconds - @animStart) / @duration
 		@actImg.draw((@view.width - @actImg.width) / 2, (@view.height - @actImg.height) / 2, Depth::TEXT, 1, 1,
+			Gosu::Color.new(alpha * 255, 255, 255, 255))
+
+		@subImg.draw((@view.width - @subImg.width) / 2, (@view.height - @subImg.height) / 2 + 100, Depth::TEXT, 1, 1,
 			Gosu::Color.new(alpha * 255, 255, 255, 255))
 	end
 
